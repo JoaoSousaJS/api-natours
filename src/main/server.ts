2745e4c39f0f35ac2fb2ee8d91da77b409c892a1
Joao <joao.rsousa23@gmail.com>
2020-09-28 21:46:23 +0930

    feat: add unhandled rejection error handler

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,11 @@
 import moongose from 'mongoose'
 import 'dotenv/config'
 import { app } from '../infra/routes/app'
+import { ProcessProtocol } from './protocols/process-protocol'
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 moongose.connect(DB, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -11,12 +13,18 @@ moongose.connect(DB, {
   useUnifiedTopology: true
 }).then(() => {
   console.log('DB connection succesfull')
-}).catch(err => {
-  console.log('Error: ', err)
 })
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server running on port ${port}...`)
+})
+
+process.on('unhandledRejection', (err: ProcessProtocol) => {
+  console.log(err.name, err.message)
+  console.log('UNHANDLED REJECTION!!Shutting down...')
+  server.close(() => {
+    process.exit(1)
+  })
 })
