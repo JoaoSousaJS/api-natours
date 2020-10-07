a2e8f09dd2ee53cdc622a075c58e86fb26e144e1
Joao <joao.rsousa23@gmail.com>
2020-10-07 21:46:50 +1030

    feat: add cookie to requests

---
+++ src/presentation/helper/authentication/cookie-option-interface.ts
@@ -0,0 +1,5 @@
+export interface CookieOption {
+  expires: Date
+  secure?: Boolean
+  httpOnly: Boolean
+}
--- src/presentation/helper/authentication/send-token-helper.ts
+++ src/presentation/helper/authentication/send-token-helper.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config'
+import { CookieOption } from './cookie-option-interface'
 import { signInJwtHelper } from './signin-helper'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const sendToken = (user, statusCode, res) => {
   const token = signInJwtHelper(user._id)
+
+  const cookieOptions: CookieOption = {
+    expires: new Date(Date.now() + Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 1000),
+    httpOnly: true
+  }
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
+
+  res.cookie('jwt', token, cookieOptions)
 
   res.status(statusCode).json({
     status: 'success',
