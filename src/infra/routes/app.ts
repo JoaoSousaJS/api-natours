f00438bb4dee1006503b9590cb0bd2a96ed75f8b
Joao <joao.rsousa23@gmail.com>
2020-10-07 23:37:29 +1030

    feat: add request limit by ip

--- src/infra/routes/app.ts
+++ src/infra/routes/app.ts
@@ -1,13 +1,26 @@
 import express from 'express'
 import morgan from 'morgan'
+import rateLimit from 'express-rate-limit'
 import { tourRouter } from './tour/tour-routes'
 import { AppError } from '../../presentation/errors/app-error'
 import { globalErrorHandler } from '../../presentation/errors/global-error-handler'
 import { userRouter } from './user/user-routes'
 
 export const app = express()
-app.use(morgan('dev'))
+
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour'
+})
+
+app.use('/api',limiter)
 
 app.use((req, res, next) => {
   // console.log(req.headers)
