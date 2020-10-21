84027e76b23124e941078c6427fb8366842850da
Joao <joao.rsousa23@gmail.com>
2020-10-21 23:18:25 +1030

    refactor: move update user to factory

--- src/presentation/controllers/user/update-user-controller.ts
+++ src/presentation/controllers/user/update-user-controller.ts
@@ -1,4 +1,5 @@
 import { UserModel } from '../../../data/models/user/user'
 import { updateOne } from '../../factory'
 
+// do not update passwords with this!
 export const updateUser = updateOne(UserModel)
