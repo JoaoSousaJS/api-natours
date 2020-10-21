bcdca40d71ae9028316d7cb34c5f2d8a4d823520
Joao <joao.rsousa23@gmail.com>
2020-10-21 23:58:22 +1030

    refactor: move create review function to factory

--- src/infra/routes/review/review-routes.ts
+++ src/infra/routes/review/review-routes.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { protectRoutes, restrictTo } from '../../../main/middlewares/'
+import { protectRoutes, restrictTo, setToursIds } from '../../../main/middlewares/'
 import { deleteReview, getAllReview, createReview, updateReview } from '../../../presentation/controllers/review'
 
 export const reviewRouter = express.Router({
   mergeParams: true
 })
 
-reviewRouter.route('/').post(protectRoutes, restrictTo('user'), createReview).get(getAllReview)
+reviewRouter.route('/').post(protectRoutes, restrictTo('user'), setToursIds, createReview).get(getAllReview)
 reviewRouter.route('/:id').delete(protectRoutes, deleteReview).patch(protectRoutes, updateReview)
--- src/presentation/controllers/review/add-review-controller.ts
+++ src/presentation/controllers/review/add-review-controller.ts
@@ -1,31 +1,4 @@
 import { ReviewModel } from '../../../data/models/review/review'
-import { catchAsync } from '../../errors'
+import { createOne } from '../../factory/create-one-factory'
 
-export const createReview = catchAsync(async (req, res, next) => {
-  // allow nested routes
-  if (!req.body.tour) req.body.tour = req.params.tourId
-  if (!req.body.user) req.body.user = req.user.id
-
-  const { user } = req.body
-  const { tour } = req.body
-
-  const { review, rating } = req.body
-  await ReviewModel.create({
-    review,
-    rating,
-    user,
-    tour
-  })
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: {
-        review,
-        rating,
-        tour,
-        user
-      }
-    }
-  })
-})
+export const createReview = createOne(ReviewModel)
