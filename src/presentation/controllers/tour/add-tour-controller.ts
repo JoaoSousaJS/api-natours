58b2eec6d402fec5200d2901391beb7d07c1349b
Joao <joao.rsousa23@gmail.com>
2020-10-21 23:44:05 +1030

    refactor: move create tour function to factory

--- src/presentation/controllers/tour/add-tour-controller.ts
+++ src/presentation/controllers/tour/add-tour-controller.ts
@@ -1,13 +1,4 @@
 import { TourModel } from '../../../data/models/tour/tour'
-import { catchAsync } from '../../errors/catch-async-error'
+import { createOne } from '../../factory/create-one-factory'
 
-export const createTour = catchAsync(async (req,res, next): Promise<void> => {
-  const newTour = await TourModel.create(req.body)
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour
-    }
-  })
-})
+export const createTour = createOne(TourModel)
