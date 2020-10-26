f8711a91727456cad679ffd5cb88db193eb04eeb
Joao <joao.rsousa23@gmail.com>
2020-10-27 09:55:21 +1030

    refactor: change import-data file to import reviews and users

--- src/data/import-data.ts
+++ src/data/import-data.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import fs from 'fs'
 import 'dotenv/config'
 import { TourModel } from './models/tour/tour'
+import { UserModel } from './models/user/user'
+import { ReviewModel } from './models/review/review'
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
@@ -18,10 +20,14 @@ mongoose.connect(DB, {
 })
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'))
 
 const importData = async (): Promise<void> => {
   try {
     await TourModel.create(tours)
+    await UserModel.create(users, { validateBeforeSave: false })
+    await ReviewModel.create(reviews)
     console.log('Data successfully loaded!')
     process.exit()
   } catch (err) {
@@ -32,6 +38,8 @@ const importData = async (): Promise<void> => {
 const deleteData = async (): Promise<void> => {
   try {
     await TourModel.deleteMany({})
+    await UserModel.deleteMany({})
+    await ReviewModel.deleteMany({})
     console.log('Data successfully deleted!')
     process.exit()
   } catch (err) {
